4999d8d664c0409c1dc2ccb1da9eb92affed3d2e
Lawrence Hu <69295147+akira7951@users.noreply.github.com>
2024-07-15 09:59:56 +0800

    Add files via upload

---
+++ pianosdk/user/models/access.py
@@ -0,0 +1,19 @@
+from datetime import date, datetime
+from pydantic.main import BaseModel
+from typing import Optional
+from pianosdk.user.models.resource import Resource
+from pianosdk.user.models.user import User
+
+
+class Access(BaseModel):
+    access_id: Optional[str]
+    parent_access_id: Optional[str]
+    granted: Optional[bool]
+    user: Optional['User']
+    resource: Optional['Resource']
+    expire_date: Optional[datetime]
+    start_date: Optional[datetime]
+    can_revoke_access: Optional[bool]
+
+
+Access.update_forward_refs()
